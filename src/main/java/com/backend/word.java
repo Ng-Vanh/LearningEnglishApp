a8606c6bd089a199732cbf2a6f5039b27427a680
Nguyễn Việt Anh <nguyenvietanh2234@gmail.com>
2023-09-22 17:45:42 +0700

    add javadoc to constructor word

--- src/main/java/com/backend/word.java
+++ src/main/java/com/backend/word.java
@@ -4,6 +4,12 @@ public class word {
     private String target;
     private String explain;
 
+    /**
+     * Constructs a new instance of the Word class.
+     * @param target The English word.
+     * @param explain The Vietnamese translation of the word.
+     */
+
     public word(String target, String explain) {
         this.target = target;
         this.explain = explain;

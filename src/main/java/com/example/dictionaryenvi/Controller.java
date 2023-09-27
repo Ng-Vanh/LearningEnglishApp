c246d22102e095f1dc75adf227715273d269d0eb
Nguyễn Việt Anh <nguyenvietanh2234@gmail.com>
2023-09-27 20:16:04 +0700

    fix error module not found

--- src/main/java/com/example/dictionaryenvi/Controller.java
+++ src/main/java/com/example/dictionaryenvi/Controller.java
@@ -2,7 +2,9 @@ package com.example.dictionaryenvi;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.web.WebView;
 
 public class Controller {
-
+    @FXML
+    private WebView showMean;
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.dictionaryenvi {
     requires com.almasb.fxgl.all;
     requires com.fasterxml.jackson.databind;
     requires java.sql;
+    requires javafx.web;
     opens com.example.dictionaryenvi to javafx.fxml;
     exports com.example.dictionaryenvi;
 }

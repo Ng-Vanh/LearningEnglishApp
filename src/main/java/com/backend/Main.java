485a522f75df27b717b2b8934081ce1378b2a2d9
Hoàng Đức Mạnh <hoangducmanh03072004@gmail.com>
2023-10-05 21:03:06 +0700

    added some import lines

--- src/main/java/com/backend/Main.java
+++ src/main/java/com/backend/Main.java
@@ -1,19 +1,24 @@
 package com.backend;
 
+import com.backend.ChatGPT.ChatGPT;
+import com.backend.Exercise.MultipleChoice;
 import com.backend.OnlineDictionary.GoogleTranslate;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        String query = scanner.nextLine();
-
+//
 //        ChatGPT bot = new ChatGPT();
 //        String query = "Generate a challenging English vocabulary practice exercise in JSON format with a sentence containing a blank in the question, four options (A, B, C, D) for completion, and a specified correct answer. JSON format: {\"question\": \"\", \"options\": {\"A\": \"\", \"B\": \"\", \"C\": \"\", \"D\": \"\"}, \"correctAnswer\": \"\"}.";
 //        String query = "Generate a challenging English vocabulary practice exercise in JSON-like format with a sentence containing a blank in the question, four options (A, B, C, D) for completion, and a specified correct answer. The format should follow this pattern: {question: , options: {A: , B: , C: , D: }, correctAnswer: }.";
 //        String response = ChatGPT.getGPTAnswer(query);
-
+//
 //        System.out.println(response);
-
-
+//
+//
 //        MultipleChoice mc = new MultipleChoice(); // tao cau hoi multiple choice
 //        mc.print();
 

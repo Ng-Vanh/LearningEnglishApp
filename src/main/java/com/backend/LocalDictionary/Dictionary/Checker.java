7d49659ecf9dd7e712f10eb37c886128e73eeea2
Drizzle210 <55441313+Drizzle210@users.noreply.github.com>
2023-10-05 23:17:24 +0700

    modify checker.java

--- src/main/java/com/backend/LocalDictionary/Dictionary/Checker.java
+++ src/main/java/com/backend/LocalDictionary/Dictionary/Checker.java
@@ -1,11 +1,14 @@
 package com.backend.LocalDictionary.Dictionary;
 public class Checker {
     public static boolean isValidWord(String word) {
-        for (int i = 0; i < word.length() ; ++i) {
-            if (!Character.isLetterOrDigit(word.charAt(i))) {
-                System.out.println(word.charAt(i));
-                return false;
-            }
+        for (int i = 0; i < word.length(); ++i) {
+            if(word.charAt(i) == '-' || word.charAt(i) == ' ')  continue;
+            char c = word.charAt(i);
+            if(c < 'a' || c > 'z')  return false;
+//            if (!Character.isLetterOrDigit(word.charAt(i))) {
+////                System.out.println(word.charAt(i));
+//                return false;
+//            }
         }
         return true;
     }

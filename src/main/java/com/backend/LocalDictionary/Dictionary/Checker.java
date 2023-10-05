8888b9ae8533ec1332a242e88b3bd99b1d4aebc4
Drizzle210 <55441313+Drizzle210@users.noreply.github.com>
2023-10-06 00:11:37 +0700

    add javadoc

--- src/main/java/com/backend/LocalDictionary/Dictionary/Checker.java
+++ src/main/java/com/backend/LocalDictionary/Dictionary/Checker.java
@@ -1,5 +1,10 @@
 package com.backend.LocalDictionary.Dictionary;
 public class Checker {
+    /**
+     *
+     * @param word
+     * @return
+     */
     public static boolean isValidWord(String word) {
         for (int i = 0; i < word.length(); ++i) {
             if(word.charAt(i) == '-' || word.charAt(i) == ' ')  continue;

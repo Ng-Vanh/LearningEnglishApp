7f19560679026876f1ab84cd697b95b89a013599
Drizzle210 <55441313+Drizzle210@users.noreply.github.com>
2023-10-22 11:51:10 +0700

    add class extends , favorite word , history search

--- src/main/java/com/backend/LocalDictionary/Dictionary/DictionaryManagement.java
+++ src/main/java/com/backend/LocalDictionary/Dictionary/DictionaryManagement.java
@@ -1,55 +1,96 @@
 package com.backend.LocalDictionary.Dictionary;
 
 import com.backend.Connection.WordDataAccess;
-import com.backend.LocalDictionary.Dictionary.Dictionary;
 import com.backend.LocalDictionary.Trie.Trie;
-import com.backend.Connection.WordDataAccess;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.backend.LocalDictionary.Dictionary.Checker.isValidWord;
 
-public class DictionaryManagement {
+public class DictionaryManagement extends Trie {
     private static final String DATA_INPUT_FILE_PATH = "src\\main\\java\\com\\backend\\LocalDictionary\\dictionaries.txt";
     private static final String DATA_OUTPUT_FILE_PATH = "src\\main\\java\\com\\backend\\LocalDictionary\\dictionaries.txt";
+    private static final String FAVORITE_WORD_FILE_PATH = "src\\main\\java\\com\\backend\\LocalDictionary\\favorite_word.txt";
+    private static final String HISTORY_SEARCH_FILE_PATH = "src\\main\\java\\com\\backend\\LocalDictionary\\history_search.txt";
     private static final String SPLITTING_CHARACTERS = "<html>";
-    Trie trie = new Trie();
-    Dictionary dictionary = new Dictionary();
-    public DictionaryManagement () {
+    private Trie trie = new Trie();
+    private Set<Word> favoriteWord = new HashSet<Word>();
+    private Set<Word> historySearch = new HashSet<Word>();
+
+    public DictionaryManagement() {
         try {
-//            readDataFromTxtFile();
-            readDataFromDatabase();
+            readDataFromTxtFile();
+            readFavoriteWordFromTxtFile();
+            readHistorySearchFromTxtFile();
+//            readDataFromDatabase();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * read data from txt file to trie.
+     *
+     * @throws IOException exception.
+     */
     public void readDataFromTxtFile() throws IOException {
         FileReader fis = new FileReader(DATA_INPUT_FILE_PATH);
         BufferedReader br = new BufferedReader(fis);
         String line;
-        while((line = br.readLine()) != null) {
+        while ((line = br.readLine()) != null) {
 //            System.out.println(line);
             String[] parts = line.split(SPLITTING_CHARACTERS);
             String target = parts[0];
             String explain = SPLITTING_CHARACTERS + parts[1];
-            insertFromCommandLine(target , explain);
+            Word word = new Word(target, explain);
+            insertWord(word);
         }
     }
+
+    public void readFavoriteWordFromTxtFile() throws IOException {
+        FileReader fis = new FileReader(FAVORITE_WORD_FILE_PATH);
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        while ((line = br.readLine()) != null) {
+//            System.out.println("Read " + line);
+            Word word = new Word(line);
+            favoriteWord.add(word);
+        }
+    }
+    public void readHistorySearchFromTxtFile() throws IOException {
+        FileReader fis = new FileReader(HISTORY_SEARCH_FILE_PATH);
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        while ((line = br.readLine()) != null) {
+//            System.out.println("Read " + line);
+            Word word = new Word(line);
+            historySearch.add(word);
+        }
+    }
+
+    /**
+     * read data from database to trie.
+     *
+     * @throws IOException
+     */
     public void readDataFromDatabase() throws IOException {
         WordDataAccess wordDataAccess = new WordDataAccess();
         ArrayList<Word> allWords = wordDataAccess.selectAll();
         for (Word word : allWords) {
-            String target = word.getTarget();
-            String explain = word.getExplain();
-            insertFromCommandLine(target , explain);
+            insertWord(word);
         }
     }
+
+    /**
+     * export data from trie to txt file.
+     */
     public void exportDataToTxtFile() {
         try {
             File file = new File(DATA_OUTPUT_FILE_PATH);
-            if(file.exists()) {
+            if (file.exists()) {
 //                System.out.println("deleted");
                 file.delete();
             }
@@ -66,54 +107,155 @@ public class DictionaryManagement {
             e.printStackTrace();
         }
     }
-    public void insertFromCommandLine(String target , String explain) {
-        target = target.toLowerCase();
-        if(isValidWord(target)) {
-            dictionary.addTo(target , explain);
-            trie.insert(dictionary.word);
-        }
-//        else {
-//            System.out.println(target + "not valid!");
-//        }
-    }
-    public void removeFromCommandLine(String target) {
-        target = target.toLowerCase();
-        if(isValidWord(target)) {
-            trie.remove(target);
-        }
-        else {
-            System.out.println("not valid!");
+
+    /**
+     * insert word to trie.
+     *
+     * @param word word.
+     */
+    @Override
+    public void insertWord(Word word) {
+        word.setTarget(word.getTarget().toLowerCase());
+        if (!isValidWord(word.getTarget())) {
+//            System.out.println(word.getTarget() + " not valid!");
+        } else {
+            super.insertWord(word);
         }
     }
-    public void updateFromCommandLine(String target , String explain) {
-        target = target.toLowerCase();
-        if(isValidWord(target)) {
-            trie.update(target , explain);
-        }
-        else {
-            System.out.println("not valid!");
+
+    /**
+     * remove word from trie.
+     *
+     * @param word word.
+     */
+    @Override
+    public void removeWord(Word word) {
+        word.setTarget(word.getTarget().toLowerCase());
+        if (!isValidWord(word.getTarget())) {
+            System.out.println(word.getTarget() + " not valid!");
+        } else {
+            super.removeWord(word);
         }
     }
-    public ArrayList<Word> getAllWords() {
-        return trie.getAllTrieWords();
-    }
-    public void showAllWord() {
-        trie.showAllWord();
-    }
-    public String dictionaryLookup(String target) {
-        target = target.toLowerCase();
-        String result = "not valid!";
-        if(isValidWord(target)) {
-            result = trie.lookup(target);
+
+    /**
+     * update word target.
+     *
+     * @param word word.
+     */
+    @Override
+    public void updateWord(Word word) {
+        word.setTarget(word.getTarget().toLowerCase());
+        if (!isValidWord(word.getTarget())) {
+            System.out.println(word.getTarget() + " not valid!");
+        } else {
+            super.updateWord(word);
         }
+    }
+
+    /**
+     * show all word from trie.
+     */
+    @Override
+    public void showAllWords() {
+        super.showAllWords();
+    }
+
+    /**
+     * look up word from trie.
+     *
+     * @param word word.
+     * @return explain of word target.
+     */
+    @Override
+    public Word lookupWord(Word word) {
+        word.setTarget(word.getTarget().toLowerCase());
+        Word result = new Word();
+        if (!isValidWord(word.getTarget())) {
+            result.setExplain(word.getTarget() + " not valid");
+        }
+        result = super.lookupWord(word);
         return result;
     }
-    public ArrayList searcher(String target) {
-        target = target.toLowerCase();
-        ArrayList<Word> result = new ArrayList<>();
-        if(isValidWord(target)) {
-            result = trie.search(target);
+
+    /**
+     * search word from trie.
+     *
+     * @param word word.
+     * @return list word result started with target.
+     */
+    @Override
+    public ArrayList<Word> searchWord(Word word) {
+        word.setTarget(word.getTarget().toLowerCase());
+        ArrayList<Word> result = new ArrayList<Word>();
+        if (!isValidWord(word.getTarget())) {
+            Word newWord = new Word();
+            newWord.setExplain(word.getTarget() + " not valid");
+            result.add(newWord);
+            return result;
         }
-        return result;
+        return super.searchWord(word);
     }
+
+    /**
+     * add favorite word to set and export to file favorite_word.txt.
+     *
+     * @param word word.
+     */
+    public void addFavoriteWord(Word word) {
+        if (!favoriteWord.contains(word)) {
+            favoriteWord.add(word);
+            try {
+                File file = new File(FAVORITE_WORD_FILE_PATH);
+                FileWriter fileWriter = new FileWriter(FAVORITE_WORD_FILE_PATH, true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                printWriter.println(word.getTarget());
+                printWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * add history search word to set and export to file history_search.txt.
+     *
+     * @param word word.
+     */
+    public void addHistorySearch(Word word) {
+        if (!historySearch.contains(word)) {
+            historySearch.add(word);
+            try {
+                File file = new File(HISTORY_SEARCH_FILE_PATH);
+                FileWriter fileWriter = new FileWriter(HISTORY_SEARCH_FILE_PATH, true);
+                PrintWriter printWriter = new PrintWriter(fileWriter);
+                printWriter.println(word.getTarget());
+                printWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public Set<Word> getFavoriteWord() {
+        return favoriteWord;
+    }
+
+    public Set<Word> getHistorySearch() {
+        return historySearch;
+    }
+
+    /**
+     * check if word is favorite or not.
+     *
+     * @param word word.
+     * @return true if favorite , false otherwise.
+     */
+    public boolean checkIsFavoriteWord(Word word) {
+        if (favoriteWord.contains(word)) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/backend/LocalDictionary/Dictionary/Word.java
+++ src/main/java/com/backend/LocalDictionary/Dictionary/Word.java
@@ -1,12 +1,28 @@
 package com.backend.LocalDictionary.Dictionary;
 
+import java.util.Objects;
+
 public class Word {
     private String target;
     private String explain;
+    private boolean isFavoriteWord;
     public Word() {
         this.target = "";
         this.explain = "";
     }
+    public Word(String target) {
+        this.target = target;
+        this.explain = "";
+    }
+
+    public boolean isFavoriteWord() {
+        return isFavoriteWord;
+    }
+
+    public void setFavoriteWord(boolean favoriteWord) {
+        isFavoriteWord = favoriteWord;
+    }
+
     /**
      * Constructs a new instance of the Word class
      * @param target The English word.
@@ -31,5 +47,21 @@ public class Word {
 
     public void setExplain(String explain) {
         this.explain = explain;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Word) {
+            Word newWord = (Word) o;
+            if(target.equals(newWord.getTarget())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isFavoriteWord);
     }
 }

c1ac751e9290bb599e0f66b718ffb9ecb6e431d1
Drizzle210 <55441313+Drizzle210@users.noreply.github.com>
2023-10-27 09:58:07 +0700

    add remove favoriteWord, login interface, signUp interface, resetPassword interface

---
+++ src/main/java/com/example/dictionaryenvi/SignUp.java
@@ -0,0 +1,39 @@
+package com.example.dictionaryenvi;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUp {
+    @FXML
+    private TextField firstname;
+    @FXML
+    private TextField lastname;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private PasswordField confirmPassword;
+    public void backToLogIn(MouseEvent mouseEvent) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Login/Login.fxml"));
+        try {
+            Parent root = loader.load();
+            Scene scene =new Scene(root);
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

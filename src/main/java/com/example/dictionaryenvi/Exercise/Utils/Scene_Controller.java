daefc1d285815eb75e26fe1571a5945379ec130c
Hoàng Đức Mạnh <hoangducmanh03072004@gmail.com>
2023-11-10 00:03:20 +0700

    cool bounce effect when switching scene

--- src/main/java/com/example/dictionaryenvi/Exercise/Utils/Scene_Controller.java
+++ src/main/java/com/example/dictionaryenvi/Exercise/Utils/Scene_Controller.java
@@ -1,7 +1,6 @@
 package com.example.dictionaryenvi.Exercise.Utils;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -21,17 +20,39 @@ public class Scene_Controller {
 
     }
 
-//    public void enter_newScene(String FXML_Path, String title, ActionEvent event) {
+//    public void enter_newScene(String FXML_Path, String title, ActionEvent event, boolean slideFromRight) {
 //        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_Path));
 //        try {
 //            Parent root = loader.load();
-//            Scene scene = new Scene(root);
-//            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//            stage.setScene(scene);
 //
-//            stage.setTitle(title);
+//            Scene currentScene = ((Node) event.getSource()).getScene();
+//            Stage stage = (Stage) currentScene.getWindow();
 //
-//            stage.show();
+//            SnapshotParameters snapshotParameters = new SnapshotParameters();
+//            snapshotParameters.setFill(javafx.scene.paint.Color.TRANSPARENT);
+//            ImageView snapshot = new ImageView(currentScene.getRoot().snapshot(snapshotParameters, null));
+//
+//            StackPane rootPane = new StackPane();
+//            rootPane.getChildren().addAll(snapshot, root);
+//
+//            double sceneWidth = currentScene.getWidth();
+//            root.translateXProperty().set(slideFromRight ? sceneWidth : -sceneWidth); // Initial position off the scene
+//
+//            Scene newScene = new Scene(rootPane, sceneWidth, currentScene.getHeight());
+//
+//            FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.01), snapshot);
+//            fadeOut.setToValue(0);
+//            fadeOut.setOnFinished(e -> {
+//                stage.setScene(newScene);
+//                stage.setTitle(title);
+//
+//                TranslateTransition transition = new TranslateTransition(Duration.seconds(0.1), root);
+//                transition.setToX(0);
+//                transition.setOnFinished(eventTransition -> rootPane.getChildren().remove(snapshot)); // Remove snapshot after transition
+//                transition.play();
+//            });
+//            fadeOut.play();
+//
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
@@ -57,16 +78,26 @@ public class Scene_Controller {
 
             Scene newScene = new Scene(rootPane, sceneWidth, currentScene.getHeight());
 
-            FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.2), snapshot);
+            FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.5), snapshot);
             fadeOut.setToValue(0);
             fadeOut.setOnFinished(e -> {
                 stage.setScene(newScene);
                 stage.setTitle(title);
 
-                TranslateTransition transition = new TranslateTransition(Duration.seconds(0.2), root);
+                TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), root);
                 transition.setToX(0);
-                transition.setOnFinished(eventTransition -> rootPane.getChildren().remove(snapshot)); // Remove snapshot after transition
                 transition.play();
+
+                // Apply a bouncing effect with acceleration and deceleration
+                transition.setOnFinished(eventTransition -> {
+                    Timeline timeline = new Timeline();
+                    KeyValue keyValue = new KeyValue(root.translateXProperty(), slideFromRight ? -30 : 30);
+                    KeyFrame keyFrame = new KeyFrame(Duration.millis(70), keyValue);
+                    timeline.getKeyFrames().add(keyFrame);
+                    timeline.setAutoReverse(true);
+                    timeline.setCycleCount(2);
+                    timeline.play();
+                });
             });
             fadeOut.play();
 
@@ -74,34 +105,4 @@ public class Scene_Controller {
             throw new RuntimeException(e);
         }
     }
-
-
-//    public void enter_newScene(String FXML_Path, String title, ActionEvent event) {
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource(FXML_Path));
-//        try {
-//            Parent root = loader.load();
-//            Scene scene = new Scene(root);
-//
-//            // Get the current stage
-//            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//
-//            // Create a translate transition
-//            TranslateTransition transition = new TranslateTransition(Duration.seconds(4), root);
-//            transition.setFromX(stage.getWidth()); // Sliding from the right (assuming stage width is the X direction)
-//            transition.setToX(0); // Sliding to the left
-//
-//            // Play the transition
-//            transition.play();
-//
-//            // Set the scene after the transition
-//            transition.setOnFinished(e -> {
-//                stage.setScene(scene);
-//                stage.setTitle(title);
-//                stage.show();
-//            });
-//
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
